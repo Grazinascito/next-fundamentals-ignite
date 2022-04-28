68a412b57fee6ca3f8c2e5ab369154e9c6a5416f
Grazinascito <grazi.social84@gmail.com>
2022-04-28 11:38:44 -0300

    finished app structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | meu site bonito</title>
+      </Head>
+      
+      <div>
+        <h1>
+          oi, linda <span>amg</span>
+        </h1>
+        <h2>cucucuc</h2>
+      </div>
+    </>
+  );
+}
